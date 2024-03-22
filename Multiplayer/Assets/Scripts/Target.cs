db497295f453782c2065d38ba6d9cc4481c04f9a
Rogueiron <112098171+Rogueiron@users.noreply.github.com>
2024-03-22 13:24:20 -0400

    test sync

--- Multiplayer/Assets/Scripts/Target.cs
+++ Multiplayer/Assets/Scripts/Target.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Alteruna;
 
-public class Target : MonoBehaviour
+public class Target : AttributesSync
 {
     private float health = 50f;
     private Spawner spawner;
+
     private void Start()
     {
         spawner = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<Spawner>();
+        
     }
 
     public void takeDamage(float amount)
@@ -20,8 +22,9 @@ public class Target : MonoBehaviour
             Die();
         }
     }
+    [SynchronizableMethod]
     void Die()
     {
-        spawner.Despawn(gameObject);
+        Destroy(gameObject);
     }
 }
